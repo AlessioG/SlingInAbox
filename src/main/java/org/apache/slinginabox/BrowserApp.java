3e10077f165bcc45c2ebb1bd9591d3d101feb8a6
onflapp <onflapp@gmail.com>
2013-07-03 08:17:42 +0200

    major reorg

--- src/main/java/org/apache/slinginabox/BrowserApp.java
+++ src/main/java/org/apache/slinginabox/BrowserApp.java
@@ -1,4 +1,4 @@
-package com.onflapp.slinginabox;
+package org.apache.slinginabox;
 
 import org.eclipse.swt.*;
 import org.eclipse.swt.layout.*;
@@ -6,54 +6,47 @@ import org.eclipse.swt.widgets.*;
 import org.eclipse.swt.browser.*;
 
 public class BrowserApp {
-	public static void main(String args[]) {
+	public static void main (String args[]) {
 		Display display = new Display();
-		final Shell shell = new Shell(display);
+		final Shell shell = new Shell (display);
 		final Browser browser;
 
 		try {
-			browser = new Browser(shell, SWT.NONE);
-		} catch (SWTError e) {
-			System.out.println("Could not instantiate Browser: " + e.getMessage());
+			browser = new Browser (shell, SWT.NONE);
+		}
+		catch (SWTError e) {
+			System.out.println ("Could not instantiate Browser: " + e.getMessage());
 			display.dispose();
 			return;
 		}
 
-		shell.setLayout(new FillLayout());
+		shell.setLayout (new FillLayout());
 
-		/*
-		final ProgressBar progressBar = new ProgressBar(shell, SWT.NONE);
-		data = new GridData();
-		data.horizontalAlignment = GridData.END;
-		progressBar.setLayoutData(data);
+		browser.addProgressListener (new ProgressListener() {
+			public void changed (ProgressEvent event) {
+				if (event.total == 0) return;
+				int ratio = event.current * 100 / event.total;
+			}
+			public void completed (ProgressEvent event) {
+				System.out.println ("done");
+			}
+		});
+		browser.addStatusTextListener (new StatusTextListener() {
+			public void changed (StatusTextEvent event) {
+				System.out.println ("status:" + event);
+			}
+		});
+		browser.addLocationListener (new LocationListener() {
+			public void changed (LocationEvent event) {
+				System.out.println ("loc:" + event);
+			}
+			public void changing (LocationEvent event) {
+			}
+		});
 
-		browser.addProgressListener(new ProgressListener() {
-			public void changed(ProgressEvent event) {
-					if (event.total == 0) return;                            
-					int ratio = event.current * 100 / event.total;
-					progressBar.setSelection(ratio);
-			}
-			public void completed(ProgressEvent event) {
-				progressBar.setSelection(0);
-			}
-		});
-		browser.addStatusTextListener(new StatusTextListener() {
-			public void changed(StatusTextEvent event) {
-				status.setText(event.text);	
-			}
-		});
-		browser.addLocationListener(new LocationListener() {
-			public void changed(LocationEvent event) {
-				if (event.top) location.setText(event.location);
-			}
-			public void changing(LocationEvent event) {
-			}
-		});
-		*/
-		
 		shell.open();
-		browser.setUrl(args[0]);
-		
+		browser.setUrl (args[0]);
+
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch())
 				display.sleep();
